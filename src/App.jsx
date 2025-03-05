f86decd878d93f4fccde760ccb1f600115cbcccd
dfi-dev <saurabh.tpa48@gmail.com>
2025-03-05 18:32:58 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Footer from './components/Footer'
+import LandingPage from './components/LandingPage';
+import Header from './components/Header';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import TopDonors from './pages/TopDonors';
+import Donate from './pages/Donate';
+import Notifications from "./components/Notifications.jsx";
+
+
+
+const App = () => {
+    return <>
+    {/*<Header></Header>*/}
+    {/*<LandingPage/>*/}
+        <Notifications/>
+     {/*<About/>*/}
+    {/* <Contact/>*/}
+    {/* <TopDonors/> */}
+    {/* <Donate/>*/}
+    <Footer/>
+    </>
+};
+
+export default App;
