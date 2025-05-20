65fe687ae48b6ca00246bef0828504ec8c764ea7
Saurabh Singh <saurabh.tpa48@gmail.com>
2025-05-20 18:12:04 +0530

    new universal file controller added

---
+++ Extra/Controller(NEW)/api.php
@@ -0,0 +1,18 @@
+
+<?php
+
+use App\Http\Controllers\ImageController;
+use App\Http\Controllers\FileController;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/welcome', function () {
+    return response()->json(['message' => 'Welcome to My Laravel API!']);
+});
+
+Route::get('/images/{filename}', [ImageController::class, 'show']);
+Route::get('/files/{path}', [FileController::class, 'show'])->where('path', '.*');
+
+
